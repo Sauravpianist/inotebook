f875db57e68745cc35bd2b4dc4bc11ecd6f26e76
Saurav <saurav.21scse1011071@galgotiasuniversity.edu.in>
2022-10-07 14:12:21 +0530

    authtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+// Creating the Secret for JWT
+const JWT_SECRET = 'sauravpianist';
 
 router.post('/createuser',[
     body('name', 'Enter Valid Name').isLength({ min: 3 }),
@@ -23,14 +27,29 @@ router.post('/createuser',[
     if(user){
       return res.status(400).json({error: "User with this email already exist"});
     }
+    const salt = await bcrypt.genSalt(10);
+    // console.log(salt);
+    const secPass =  await bcrypt.hash(req.body.password, salt);
 
     //create a new user
      user = await User.create({
         name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+        email: req.body.password,
+        password: secPass,
       })
-      res.json(user);
+      const data = {
+        user:{
+          id: user.id
+        }
+      }
+
+      //Authentication by using Json Web Token
+
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      // console.log(jwtData);
+      // res.json(user);
+      res.json({authtoken});
+
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occured");
