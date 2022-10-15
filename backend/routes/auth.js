327604ca90252531c9550ad2f3817f33931ffd13
Saurav <saurav.21scse1011071@galgotiasuniversity.edu.in>
2022-10-15 15:00:01 +0530

    login End Point

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -34,7 +34,7 @@ router.post('/createuser',[
     //create a new user
      user = await User.create({
         name: req.body.name,
-        email: req.body.password,
+        email: req.body.email,
         password: secPass,
       })
       const data = {
@@ -55,6 +55,53 @@ router.post('/createuser',[
       res.status(500).send("some error occured");
     }
 
-      } );
+  } );
+
+  // Authenticate the user using: Post "/api/auth/login" . No-login required
+
+  router.post('/login',[
+    body('email', 'Enter valid email address').isEmail(),
+    body('password', 'Password can not be blank').exists()
+] , async(req, res)=>{
+    // If there are errors, return bad request and the error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+  const {email, password} = req.body;
+  try { 
+    let user = await User.findOne({email});
+    console.log(user);
+    if(!user){
+      return res.status(400).json({error: "Please Enter a valid email"});
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if(!passwordCompare){
+      return res.status(400).json({error: "Please Enter a valid Credentials"});
+    }
+
+    const data = {
+      user:{
+        id: user.id
+      }
+    }
+
+    const authtoken = jwt.sign(data, JWT_SECRET);
+      // console.log(jwtData);
+      // res.json(user);
+      res.json({authtoken});
+
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server error");
+  }
+
+
+});
+
+  
 
 module.exports = router;
